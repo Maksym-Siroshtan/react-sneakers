a5ee7ced4b96debe986f0795c44de4a83b6998a1
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-03-07 20:54:12 +0200

    Added search feature.

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,27 @@ import CardList from "./components/CardList";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const fetchItems = async () => {
+    let title = `*${searchQuery}*`;
+
+    const { data } = await axios.get(
+      "https://757ed0bbb74e1c15.mokky.dev/items",
+      {
+        params: { title },
+      }
+    );
+    setItems(data);
+  };
 
   useEffect(() => {
-    const fetchItems = async () => {
-      const { data } = await axios.get(
-        "https://8ca1b7098d059351.mokky.dev/items"
-      );
-      setItems(data);
-    };
-
     fetchItems();
   }, []);
+
+  useEffect(() => {
+    fetchItems();
+  }, [searchQuery]);
 
   return (
     <div className="max-w-5xl mx-auto bg-white rounded-3xl m-20">
@@ -28,6 +38,8 @@ function App() {
           <div className="relative flex items-center">
             <img src="/search.svg" alt="Search" className="absolute left-3" />
             <input
+              onChange={() => setSearchQuery(event.target.value)}
+              value={searchQuery}
               type="text"
               placeholder="Пошук..."
               className="pl-10 py-2 pr-4 outline-none border border-gray-200 rounded-lg overflow-hidden focus:border-gray-600 placeholder:text-xs"
